2542c8e253f69ec2021624aa59d23148214ef132
Ian Wood <ian.wood108@gmail.com>
2017-10-26 22:49:21 -0400

    Start with mobile seed and update to 5.5.4

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+//Literally just a timer so I can display the logo for a while
+
+public class Load : MonoBehaviour {
+
+	public float loadTime = 5;
+	private float currentTime;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - currentTime >= loadTime){
+			SceneManager.LoadScene ("MenuScene");
+		}
+	}
+}
